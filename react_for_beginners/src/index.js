05604ad858400ad1aa1813a5b26de989d1f4762b
kimberlyydanij <kimberlydanij@gmail.com>
2023-05-20 04:25:54 +0900

    20230520 | Update

--- react_for_beginners/src/index.js
+++ react_for_beginners/src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+document.body.style.backgroundColor = 'purple';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function windowSize(event) {
+  const size = window.innerWidth;
+  if (size >= 900) {
+    document.body.style.backgroundColor = 'yellow';
+  } else if (size <= 600) {
+    document.body.style.backgroundColor = 'blue';
+  } else document.body.style.backgroundColor = 'purple';
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+window.addEventListener('resize', windowSize);
